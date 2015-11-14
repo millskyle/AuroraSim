761528f84d90e48b8cb367ee63a783659ec15a36
Kyle Mills <kyle.mills@uoit.net>
2015-11-14 00:24:03 -0500

    main time advancement skeleton added

--- src/main.cpp
+++ src/main.cpp
@@ -16,90 +16,69 @@ using namespace std;
 
 
 
-
 int main() {
-srand ( time(NULL));
-
-//random_emission_distribution();
-
-vector<float> randoms;
-vector<float> box_size;
-box_size.push_back(100.00);
-box_size.push_back(100.00);
-box_size.push_back(200.00);
-
-int N=100;
-int dead=0;
-float E_lost_interaction=1.0;
-float E_low_threshold = 10;
-
-float dt = 0.001;
-
-float velocity_scale = 10.0;
-float x[N];
-float y[N];
-float z[N];
-
-float vx[N];
-float vy[N];
-float vz[N];
-
-float E[N];
-
-float m_e = 1.0;
-
-for (int i = 0; i < N; i++) {
-   x[i] = box_size[0] * (float)rand() / RAND_MAX;
-   y[i] = box_size[1] * (float)rand() / RAND_MAX;
-   z[i] = box_size[2] * 2;
-   randoms = gen_random(3);
-  
-   vx[i] = 0.001 * velocity_scale * randoms[0];
-   vy[i] = 0.001 * velocity_scale * randoms[1];
-   vz[i] = velocity_scale * randoms[2];
-  
-   E[i] = 0.5 * m_e * (vx[i]*vx[i] + vy[i]*vy[i] + vz[i]*vz[i] );
-}
-
-for (int t=0; t< 100; t++ ) { 
-//   cout << "test" << endl;i
-   for (int i=0; i< N ; i++) {
-
-      E[i] -= E_lost_interaction;
-
-      
-      
-      x[i] += vx[i] * dt; 
-      y[i] += vy[i] * dt;
-      z[i] += vz[i] * dt;
-     
-      E[i] = 0.5 * m_e * (vx[i]*vx[i] + vy[i]*vy[i] + vz[i]*vz[i] );
+   srand ( time(NULL));
+   vector<float> randoms;
+   Simulation sim;
 
 
+   for (int i=0; i<sim.N; i++) {
+      Electron ee;
+      ee.reset(&sim);
+      ee.sim = &sim;
+      sim.electrons.push_back(ee);
+      cout << i <<endl;
 
-      if (z[i] < 0 || E[i] < E_low_threshold) {
-         cout << "DEATH! " << dead << " dead" << endl;
-         x[i] = box_size[0] * (float)rand() / RAND_MAX;
-         y[i] = box_size[1] * (float)rand() / RAND_MAX;
-         z[i] = box_size[2] * 2;
-         randoms = gen_random(3);
-        
-         vx[i] = 0.001 * velocity_scale * randoms[0];
-         vy[i] = 0.001 * velocity_scale * randoms[1];
-         vz[i] = velocity_scale * randoms[2];
-        
-         E[i] = 0.5 * m_e * (vx[i]*vx[i] + vy[i]*vy[i] + vz[i]*vz[i] );
+   }
+
+
+   float rnd;
+   float p_interaction = 0.1;
+
+
+   for (int t=0; t< sim.tmax; t++ ) {
+
+      for (int i=0; i<sim.N ; i++) {
+   cout << "here " << t<< ":"<<i<<"  " << endl;
+         Electron* e = &sim.electrons[i]; //make a pointer to the electron we're dealing with
+         rnd = (float)rand()/RAND_MAX;
+         if (rnd < e->get_p_interaction() ) {
+            cout << "emitting:";
+            rnd = (float)rand()/RAND_MAX;
+            if (rnd < e->get_p_emit_red()) {
+               //emit red
+               e->emitting = 1;
+               e->emitting_time_left = e->get_t_emit_red();
+               e->emitting_wavelength = sim.wavelength_red;
+            } else if (rnd < (e->get_p_emit_red() + e->get_p_emit_green()) ) {
+               //emit green
+               e->emitting = 1;
+               e->emitting_time_left = e->get_t_emit_green();
+               e->emitting_wavelength = sim.wavelength_green;
+            } else {
+               //emit blue
+               e->emitting = 1;
+               e->emitting_time_left = e->get_t_emit_blue();
+               e->emitting_wavelength = sim.wavelength_blue;
+               cout << "blue emission, ";
+            }
+
+         }
+
          
-         dead+=1;
-      }
 
+         
+         
+         
+         
+      }
 
    }
 
 
 
 
-}
+
 
 
 
