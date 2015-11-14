f5e83783b4273895ec22d1baaa2a60f7db3baae4
Kyle Mills <kyle.mills@uoit.net>
2015-11-14 15:02:17 -0500

    main loop working, integrating positions.

--- src/main.cpp
+++ src/main.cpp
@@ -20,15 +20,23 @@ int main() {
    srand ( time(NULL));
    vector<float> randoms;
    Simulation sim;
+   PhotonDensity photon_density;
+   
+   unsigned int voxelx,voxely,voxelz;
 
 
    for (int i=0; i<sim.N; i++) {
       Electron ee;
-      ee.reset(&sim);
       ee.sim = &sim;
+      ee.reset();
+      ee.ID = i;
       sim.electrons.push_back(ee);
 
    }
+   cout << "Electron objects created" << endl;
+ 
+   ofstream debug_xyz;
+   debug_xyz.open("out.xyz");
 
 
    float rnd;
@@ -36,9 +44,12 @@ int main() {
 
 
    for (int t=0; t< sim.tmax; t++ ) {
+      debug_xyz << sim.N << "\n t=" << t << endl;
+      
 
       for (int i=0; i<sim.N ; i++) {
          Electron* e = &sim.electrons[i]; //make a pointer to the electron we're dealing with
+         debug_xyz << " A " << e->x << " " << e->y << " " << e->z << "\n" ;
          rnd = (float)rand()/RAND_MAX;
          if (rnd < e->get_p_interaction() ) {
             rnd = (float)rand()/RAND_MAX;
@@ -61,21 +72,52 @@ int main() {
 
          }
 
-         //Check to see if electrom will emit energy this timestep:
+         //Check to see if electron will emit energy this timestep:
          if (e->emitting==1) {
             e->E -= sim.hplanck * sim.clight / e->emitting_wavelength;
             e->emitting_time_left -= 1;
-            if (e->emitting_time_left < 1) {
+
+            voxelx = floor((float)photon_density.resolution_x / (float)sim.box_sizex * e->x);
+            voxely = floor((float)photon_density.resolution_y / (float)sim.box_sizey * e->y);
+            voxelz = floor((float)photon_density.resolution_z / (float)sim.box_sizez * e->z);
+
+
+
+            if (e->emitting_wavelength == sim.wavelength_red ) {
+                photon_density.incr_element(photon_density.R, voxelx,voxely,voxelz) ;
+            } else if (e->emitting_wavelength == sim.wavelength_green ) {
+                photon_density.incr_element(photon_density.G, voxelx,voxely,voxelz) ;
+            } else if (e->emitting_wavelength == sim.wavelength_blue ) {
+                photon_density.incr_element(photon_density.B, voxelx,voxely,voxelz) ;
+            }
+
+            
+            if (e->emitting_time_left < 1) { //if it's done emitting
                e->emitting = 0;
-               cout << e->emitting_wavelength << "nm  no longer emitting" << endl;
                e->emitting_wavelength = 0;
             }
-         
-
-
+         }
+         //if electron has exited the bottom of the cell, or has not enough energy remaining.
+         if (e->z < 0 || e->E < (sim.hplanck * sim.clight / e->sim->wavelength_red ) ) {
+            e->reset();
+            e->t = t;
+//            cout << "resetting electron" << endl;
          }
 
+
+         //add any applicable forces to e->Fx, e->Fy, e->Fz :
+
          
+         
+         //Perform equation of motion integration:
+
+         e->vx += 0.5 * (e->Fx / sim.m_e) * sim.dt*sim.dt ;
+         e->vy += 0.5 * (e->Fy / sim.m_e) * sim.dt*sim.dt ;
+         e->vz += 0.5 * (e->Fz / sim.m_e) * sim.dt*sim.dt ;
+
+         e->x += e->vx * sim.dt;
+         e->y += e->vy * sim.dt;
+         e->z += e->vz * sim.dt;
 
          
          
@@ -88,7 +130,7 @@ int main() {
 
 
 
-
+debug_xyz.close();
 
 
 
