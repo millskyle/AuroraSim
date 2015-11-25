9643a23ce6d58f2a12fd4b5a522fb7e2bfdd51f3
Kyle Mills <kyle.mills@uoit.net>
2015-11-25 08:41:03 -0500

    Fixed continuous creation of FFTW plans

--- src/electrostatics.cpp
+++ src/electrostatics.cpp
@@ -69,23 +69,34 @@ class ElectricField {
       float Lz;
       Simulation *sim;
       ChargeDensity *rho;
-      float *Ex ;//= new float[Nx*Ny*Nz];
-      float *Ey ;//= new float[Nx*Ny*Nz];
-      float *Ez ;//= new float[Nx*Ny*Nz];
-      fftw_complex *in ;// = new fftw_complex[Nx*Ny*Nz];
-      fftw_complex *out  ;// = new fftw_complex[Nx*Ny*Nz];
-      fftw_complex *in2  ;// = new fftw_complex[Nx*Ny*Nz];
-      fftw_complex *out2  ;// = new fftw_complex[Nx*Ny*Nz];
+      float *Ex = NULL ;//= new float[Nx*Ny*Nz];
+      float *Ey  = NULL ;//= new float[Nx*Ny*Nz];
+      float *Ez = NULL  ;//= new float[Nx*Ny*Nz];
+      fftw_complex *in = NULL  ;// = new fftw_complex[Nx*Ny*Nz];
+      fftw_complex *out = NULL   ;// = new fftw_complex[Nx*Ny*Nz];
+      fftw_complex *in2 = NULL   ;// = new fftw_complex[Nx*Ny*Nz];
+      fftw_complex *out2 = NULL   ;// = new fftw_complex[Nx*Ny*Nz];
 
-      fftw_complex *inPhiX  ;// = new fftw_complex[Nx*Ny*Nz];
-      fftw_complex *outEX  ;// = new fftw_complex[Nx*Ny*Nz];
+      fftw_complex *inPhiX = NULL   ;// = new fftw_complex[Nx*Ny*Nz];
+      fftw_complex *outEX = NULL   ;// = new fftw_complex[Nx*Ny*Nz];
 
-      fftw_complex *inPhiY  ;// = new fftw_complex[Nx*Ny*Nz];
-      fftw_complex *outEY  ;// = new fftw_complex[Nx*Ny*Nz];
+      fftw_complex *inPhiY = NULL   ;// = new fftw_complex[Nx*Ny*Nz];
+      fftw_complex *outEY  = NULL  ;// = new fftw_complex[Nx*Ny*Nz];
 
-      fftw_complex *inPhiZ  ;// = new fftw_complex[Nx*Ny*Nz];
-      fftw_complex *outEZ  ;// = new fftw_complex[Nx*Ny*Nz];
+      fftw_complex *inPhiZ = NULL   ;// = new fftw_complex[Nx*Ny*Nz];
+      fftw_complex *outEZ  = NULL  ;// = new fftw_complex[Nx*Ny*Nz];
+      float dx;
+      float dy ;
+      float dz ;
 
+      float invNx ;
+      float invNy ;
+      float invNz ;
+
+      fftw_plan forward;
+      fftw_plan inverseX;
+      fftw_plan inverseY;
+      fftw_plan inverseZ;
 
       int init(Simulation *simm, ChargeDensity *rhoo) {
          sim = simm;
@@ -95,17 +106,19 @@ class ElectricField {
          Nz = rho->resolution_z ;
          Lx = sim->box_sizex ; 
          Ly = sim->box_sizey ;
-         Lz = sim->box_sizez ;
+         Lz = sim->box_sizez ;        
+     
+         dx = Lx/float(Nx-1) ;
+         dy = Ly/float(Ny-1) ;
+         dz = Lz/float(Nz-1) ;
 
-         
-                 
-         
-         
+         invNx = 1.0/(Nx*Nx*Nx);
+         invNy = 1.0/(Ny*Ny*Ny);
+         invNz = 1.0/(Nz*Nz*Nz);
          
          Ex = new float[Nx*Ny*Nz];
          Ey = new float[Nx*Ny*Nz];
          Ez = new float[Nx*Ny*Nz];
-         planned = 0;
          in = new fftw_complex[Nx*Ny*Nz];
          out = new fftw_complex[Nx*Ny*Nz];
          in2 = new fftw_complex[Nx*Ny*Nz];
@@ -120,9 +133,14 @@ class ElectricField {
          inPhiZ = new fftw_complex[Nx*Ny*Nz];
          outEZ = new fftw_complex[Nx*Ny*Nz];
 
+         forward = fftw_plan_dft_3d(Nx,Ny,Nz,in, out, FFTW_FORWARD, FFTW_MEASURE );
+         inverseX = fftw_plan_dft_3d(Nx,Ny,Nz,inPhiX, outEX, FFTW_BACKWARD, FFTW_MEASURE );
+         inverseY = fftw_plan_dft_3d(Nx,Ny,Nz,inPhiY, outEY, FFTW_BACKWARD, FFTW_MEASURE );
+         inverseZ = fftw_plan_dft_3d(Nx,Ny,Nz,inPhiZ, outEZ, FFTW_BACKWARD, FFTW_MEASURE );
+        
+
 
       }
-
 
       int update_element(float* A, int i, int j, int k, float val) {
          A[i + Nx *( j + Ny * k) ] = val ;
@@ -138,42 +156,19 @@ class ElectricField {
    
       int compute() {
 
-         float dx = Lx/float(Nx-1) ;
-         float dy = Ly/float(Ny-1) ;
-         float dz = Lz/float(Nz-1) ;
-
-         float invNx = 1.0/(Nx*Nx*Nx);
-         float invNy = 1.0/(Ny*Ny*Ny);
-         float invNz = 1.0/(Nz*Nz*Nz);
-
          ofstream debugfile;
          debugfile.open("fftw.plt");
 
 
          double elem[2] = {0};
 
-         fftw_plan forward;
-         fftw_plan inverse;
-         fftw_plan inverseX;
-         fftw_plan inverseY;
-         fftw_plan inverseZ;
-
-         int element = -1;
+                  int element = -1;
 
          float wavenumber_x = 0;
          float wavenumber_y = 0;
          float wavenumber_z = 0;
 
-         if (!planned) {
-            forward = fftw_plan_dft_3d(Nx,Ny,Nz,in, out, FFTW_FORWARD, FFTW_MEASURE );
-//            inverse = fftw_plan_dft_3d(Nx,Ny,Nz,in2, out2, FFTW_BACKWARD, FFTW_MEASURE );
-
-            inverseX = fftw_plan_dft_3d(Nx,Ny,Nz,inPhiX, outEX, FFTW_BACKWARD, FFTW_MEASURE );
-            inverseY = fftw_plan_dft_3d(Nx,Ny,Nz,inPhiY, outEY, FFTW_BACKWARD, FFTW_MEASURE );
-            inverseZ = fftw_plan_dft_3d(Nx,Ny,Nz,inPhiZ, outEZ, FFTW_BACKWARD, FFTW_MEASURE );
-         }
-        
-
+         
          ofstream rho_out;
          rho_out.open("rho/rho_" + to_string(sim->t) + ".dat");
 
@@ -261,10 +256,10 @@ class ElectricField {
           }
        }
 
-      fftw_destroy_plan(forward);
-      fftw_destroy_plan(inverseX);
-      fftw_destroy_plan(inverseY);
-      fftw_destroy_plan(inverseZ);
+//      fftw_destroy_plan(forward);
+//      fftw_destroy_plan(inverseX);
+//      fftw_destroy_plan(inverseY);
+//      fftw_destroy_plan(inverseZ);
 rho_out.close();
 //if (sim->t==25) { exit(1); }
 }
