934e1ac7b5c3200a8504cdd69f2c137ca3ea6be1
Kyle Mills <kyle.mills@uoit.net>
2015-11-30 14:31:01 -0500

    image generation python script now interpolates frames (linearly via simple fade)

--- src/image_set_create.py
+++ src/image_set_create.py
@@ -6,11 +6,23 @@ import scipy.ndimage as ndimage
 import sys
 import cv2
 import re
+import Image
 
 brightness = sys.argv[1]
 
 
+alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n']
 
+
+def Interpolate(file1,file2):
+   a = Image.open(file1)
+   b = Image.open(file2)
+   a = a.convert("RGBA")
+   b = b.convert("RGBA")
+   for i in range(1,10):
+      new = Image.blend(a,b,i/10.0)
+      print "Writing {0}{1}.png".format(file1.split(".")[0],alpha[i])
+      new.save("{0}{1}.png".format(file1.split(".")[0],alpha[i]))
 
 
 
@@ -31,13 +43,22 @@ def convert_to_image(dat):
          plt.imshow(y,origin='lower',extent=(0,w,0,h))
          plt.savefig("output_{0}.png".format(number.zfill(8)))
          plt.close()
+         return "output_{0}.png".format(number.zfill(8))
 
+previous_file=""
+thisFile = ""
 
-
-for dat in os.listdir("output"):
+for dat in sorted(os.listdir("output"), key= lambda x: int(("0" + x.split('.')[0].zfill(8)))) :
+   print dat
    if dat.endswith(".dat"):
       try:
-         convert_to_image(dat)
+
+          this_file = convert_to_image(dat)
+          if previous_file<>"":
+             Interpolate(previous_file,this_file)
+
+          previous_file = this_file
+
       except:
          pass
 
