e6d829ad9acac0b874ab77320d43130480f309a0
kmills <kyle.mills@uoit.net>
2015-12-08 21:31:27 -0500

    cleaned up simulation.h

--- src/simulation.h
+++ src/simulation.h
@@ -17,54 +17,48 @@ class Electron ;
 
 class Simulation {
    public: 
-      int k = 21;  //number of particles will be N = 2^k 
-      int tmax = 5000;
+      int k = 20;  //number of particles will be N = 2^k 
+      int tmax = 10000;
       int t = 0;
 
-      /* box size, in km */
-      float box_sizex = 200.0; //.push_back(100.00);
-      float box_sizey = 200.0; //.push_back(100.00);
-      float box_sizez = 200.0; //push_back(200.00);
+      /* box size, in 'kilometers' */
+      float box_sizex = 200.0; 
+      float box_sizey = 200.0; 
+      float box_sizez = 200.0; 
 
       int E_field_recalc = 1;  //how many timesteps between E_field recalculations.
 
-      float dt=0.001;
+      float dt=0.001; //timestep
    
       /* Mean energy of electrons when they begin, in keV */
-      float E_mean = 10.0;
-      float E_loss_factor = 100.0;
+      float E_mean = 8.0;
+      float E_loss_factor = 100.0; //how much energy (factor) a photon emission costs.
       
       float E_low_threshold = 10; 
       
-//      int timescale_red_emission = 1100.; //110/dt;
-//      int timescale_green_emission = 50.;// 0.7/dt;
-//      int timescale_blue_emission = 2.;// 0.001/dt;
-      
-      int timescale_red_emission = 500.; //110/dt;
-      int timescale_green_emission = 100.;// 0.7/dt;
-      int timescale_blue_emission = 50.;// 0.001/dt;
+      //relative timescales for excited states
+      int timescale_red_emission = 500.; 
+      int timescale_green_emission = 100.;
+      int timescale_blue_emission = 50.;
 
-
+      //wavelength of the red emisison. Used to scale energy emission values (E=hc/lambda)
       float wavelength_red = 500.0;
       float wavelength_green = 600.0;
       float wavelength_blue = 800.0;
+
+
+      //physical constants
       float e_chg = 1e3; // elementary charge
       float epsilon_naught = 1e-9;
+      const float m_e = 1.0;
+      float hc = 1.23984197 *10000; //*plank's constant * speed of light 
       
       vector<Electron> electrons;
-
-      const float m_e = 1.0;
-      float hc = 1.23984197 *10000; //*plank's constant in keV*nm 
-
 
       int N;
 
       void init() {
          N = pow(2,k);
-//         for (int i=0; i<10000; i++) {
-//            sheet_seeds.push_back((float)rand()/RAND_MAX) ; // = gen_random(100);
-//         }
-
       }
 
       float number_density(float h) {
@@ -73,13 +67,9 @@ class Simulation {
 
 
       float bethe_energy_loss(float h,float v) {
-         return 1e13/pow((h+190.0),4);
          // dE/dx due to atmospheric 'drag'
-         float effective_Z = 7.2; // 
-         float Ionization = 92.0;
-         float coeff = 4*M_PI*number_density(h+100)*pow(e_chg,4)*effective_Z / (m_e * pow(v,2));
-         float parenthesis= log(  2*pow(v,2) / (m_e*Ionization  )) - 1.2329;
-         return coeff*parenthesis;
+         //function that could be written to compute atmospheric drag, but I've incorporated this
+         //pretty well into the probabilities
       }
 
  };
